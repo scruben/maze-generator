5573e0c54ebb68ad791e13279420b83a385ab230
Ruben Sanz <info@rubensanz.es>
2016-10-11 13:46:55 +0200

    v1.0.1

--- maze_generator.js
+++ maze_generator.js
@@ -64,41 +64,40 @@ function emptyArray(row,col) {
   for (var i = 0; i < row; i++) {
     var temp =[];
     for (var j = 0; j < col; j++) {
-      temp.push([0,0,0,0,0]);
+      temp.push([0,0,0,0,0]); // [left,up,right,down,visited]
     }
     output.push(temp);
   }
   return output;
 }
 
-function emptyMaze(row,col) {
-  var output = [];
-  for (var i = 0; i < 2*row+1; i++) {
-    var temp =[];
-    for (var j = 0; j < 2*col+1; j++) {
+
+function renderMaze(maze) {
+  // creates an empty maze
+  var render = [];
+  for (let i = 0; i < 2*maze.length+1; i++) {
+    let temp =[];
+    for (let j = 0; j < 2*maze[0].length+1; j++) {
       if (i%2===1 && j%2===1) temp.push('   ');
       else temp.push(' * ');
     }
-    output.push(temp);
+    render.push(temp);
   }
-  return output;
-}
-
-function renderMaze(maze) {
-  var render = emptyMaze(maze.length, maze[0].length);
-  for (var i = 0; i < maze.length; i++) {
-    for (var j = 0; j < maze[0].length; j++) {
+  // breaks the right walls
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[0].length; j++) {
       if (maze[i][j][0] ===1) render[2*i+1][2*j] = '   ';
       if (maze[i][j][1] ===1) render[2*i][2*j+1] = '   ';
       if (maze[i][j][2] ===1) render[2*i+1][2*j+2] = '   ';
       if (maze[i][j][3] ===1) render[2*i+2][2*j+1] = '   ';
     }
   }
-  var output = '';
+  // puts the maze in a string
+  let strOutput = '';
   for (let i = 0; i < render.length; i++) {
-    output += render[i].join('')+'\n';
+    strOutput += render[i].join('')+'\n';
   }
-  return output;
+  return strOutput;
 }
 
-console.log(createMaze(15,40));
+console.log(createMaze(10,40));
